699b38e03f71e8d00ef9b39592ed856ce23a11b7
Itamar <itamarlucio19@gmail.com>
2022-11-15 17:55:32 -0300

    scoreboard working.

---
+++ index.js
@@ -0,0 +1,29 @@
+let scoreHome = 0
+let scoreGuest = 0
+homeScore.textContent = scoreHome
+guestScore.textContent = scoreGuest
+
+function plusOneHome() {
+  scoreHome += 1
+  homeScore.textContent = scoreHome 
+}
+function plusTwoHome() {
+  scoreHome += 2
+  homeScore.textContent = scoreHome 
+}
+function plusThreeHome() {
+  scoreHome += 3
+  homeScore.textContent = scoreHome 
+}
+function plusOneGuest() {
+  scoreGuest += 1
+  guestScore.textContent = scoreGuest 
+}
+function plusTwoGuest() {
+  scoreGuest += 2
+  guestScore.textContent = scoreGuest 
+}
+function plusThreeGuest() {
+  scoreGuest += 3
+  guestScore.textContent = scoreGuest 
+}
